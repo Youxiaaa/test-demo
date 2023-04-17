d4468f90fe44085288778f33890a2f5237fa1461
Ryan <twquannon@gmail.com>
2023-04-17 15:15:23 +0800

    update

--- js/audio.js
+++ js/audio.js
@@ -1,23 +1,111 @@
-const audio = document.getElementById('myAudio');
+const play__btn = document.querySelector('#play-btn');
+const pause__btn = document.querySelector('#pause-btn');
+const resume__btn = document.querySelector('#resume-btn');
+const volumeInput = document.getElementById('volume');
+const speech__text = document.querySelector('.speech__container').textContent;
+let pause__text = speech__text
 
-function playAudio() {
-  audio.play();
+let utterance;
+let currentPosition = 0;
+
+let rateRef = 1;
+
+function playSpeech() {
+  utterance = new SpeechSynthesisUtterance(speech__text);
+  utterance.lang = 'zh-TW';
+  utterance.volume = parseFloat(volumeInput.value);
+  utterance.rate = parseFloat(rateRef);
+
+  utterance.onboundary = (event) => {
+    if (event.name === 'word') {
+      currentPosition = event.charIndex;
+    }
+  };
+
+  speechSynthesis.speak(utterance);
+
+  play__btn.style.display = 'none'
+  pause__btn.style.display = 'block'
 }
 
-function pauseAudio() {
-  audio.pause();
+function pauseSpeech() {
+  speechSynthesis.pause();
+
+  pause__btn.style.display = 'none'
+  resume__btn.style.display = 'block'
 }
 
-function changeSpeed(speed) {
-  audio.playbackRate = speed;
+function resumeSpeech() {
+  speechSynthesis.resume();
+
+  pause__btn.style.display = 'block'
+  resume__btn.style.display = 'none'
 }
 
-function changeVolume(volume) {
-  audio.volume = volume;
+function stopSpeech() {
+  speechSynthesis.cancel();
+
+  play__btn.style.display = 'block'
+  pause__btn.style.display = 'none'
+  resume__btn.style.display = 'none'
 }
 
-function setupVolume() {
-  audio.volume = 0.5
+function updateSpeech() {  
+  const volume = parseFloat(volumeInput.value);
+  const rate = parseFloat(rateRef);
+  
+  if (speechSynthesis.speaking) {
+    // Cancel the current speech
+    speechSynthesis.cancel();
+
+    // Get the remaining text
+    pause__text = speech__text.slice(currentPosition);
+
+    // Create a new utterance with the remaining text
+    const newUtterance = new SpeechSynthesisUtterance(pause__text);
+    newUtterance.lang = 'zh-TW';
+    newUtterance.volume = volume;
+    newUtterance.rate = rate;
+
+    // Set the onboundary event handler for the newUtterance
+    newUtterance.onboundary = (event) => {
+      if (event.name === 'word') {
+        currentPosition = event.charIndex;
+      }
+    };
+
+    // Speak the new utterance
+    speechSynthesis.speak(newUtterance);
+  }
 }
 
-setupVolume()
+function updateRate(num) {
+  rateRef = num;
+
+  const volume = parseFloat(volumeInput.value);
+  const rate = parseFloat(rateRef);
+  
+  if (speechSynthesis.speaking) {
+    // Cancel the current speech
+    speechSynthesis.cancel();
+
+    // Get the remaining text
+    pause__text = speech__text.slice(currentPosition);
+
+    // Create a new utterance with the remaining text
+    const newUtterance = new SpeechSynthesisUtterance(pause__text);
+    newUtterance.lang = 'zh-TW';
+    newUtterance.volume = volume;
+    newUtterance.rate = rate;
+
+    // Set the onboundary event handler for the newUtterance
+    newUtterance.onboundary = (event) => {
+      if (event.name === 'word') {
+        currentPosition = event.charIndex;
+      }
+    };
+
+    // Speak the new utterance
+    speechSynthesis.speak(newUtterance);
+  }
+}
